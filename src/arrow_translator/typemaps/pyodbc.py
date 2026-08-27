ff9e47d46f7bff82dae6a08d48c57ddef8514fcb
Dimos Konstantinos <konstantinos.dimos@themart.gr>
2026-08-27 16:22:37 +0300

    initial commit

---
+++ src/arrow_translator/typemaps/pyodbc.py
@@ -0,0 +1,17 @@
+"""
+OIDs for ODBC Driver of Microsoft SQL Server mapped to Apache Arrow(PyArrow) datatypes
+"""
+
+import pyarrow as pa
+
+TYPE_MAPS =  {
+        bool: pa.bool_(),
+        int: pa.int64(),
+        float: pa.float64(),
+        str: pa.string(),
+        bytes: pa.binary(),
+        "datetime": pa.timestamp("us"),
+        "date": pa.date32(),
+        "time": pa.time64("us"),
+        "decimal": pa.decimal128(38, 10),
+    }
