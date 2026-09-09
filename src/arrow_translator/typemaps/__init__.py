0d863f054c5e4ddd70f78e996cc98d03bfef83fa
Dimos Konstantinos <konstantinos.dimos@themart.gr>
2026-09-09 10:03:39 +0300

    added support for psycopg3 driver

--- src/arrow_translator/typemaps/__init__.py
+++ src/arrow_translator/typemaps/__init__.py
@@ -15,6 +15,8 @@ from .oracledb import DIALECT as ORCL_DIALECT
 from .oracledb import TYPE_MAPS as ORCL_TYPES
 from .psycopg2 import DIALECT as PSYCOPG2_DIALECT
 from .psycopg2 import TYPE_MAPS as PSYCOPG2_TYPES
+from .psycopg3 import DIALECT as PSYCOPG3_DIALECT
+from .psycopg3 import TYPE_MAPS as PSYCOPG3_TYPES
 from .pymssql import DIALECT as MSSQL_DIALECT
 from .pymssql import TYPE_MAPS as MSSQL_TYPES
 from .pymysql import DIALECT as PYMYSQL_DIALECT
@@ -34,13 +36,20 @@ class Lexicon:
         CX_ORACLE_DIALECT: CX_ORACLE_TYPES,
         ORCL_DIALECT: ORCL_TYPES,
         HDBCLI_DIALECT: HDBCLI_TYPES,
+        PSYCOPG3_DIALECT: PSYCOPG3_TYPES,
     }
 
     @classmethod
-    def to_pyarrow(cls, dialect: str, oid: Any):
-        return cls.typemaps[dialect][oid]
+    def to_pyarrow(cls, driver: str, oid: Any):
+        return cls.typemaps[driver][oid]
 
     @property
     @classmethod
     def available_drivers(cls):
         return cls.typemaps.keys()
+
+    @classmethod
+    def get_driver(cls, driver: str):
+        if driver not in cls.typemaps.keys():
+            raise KeyError("The provided driver doesn't exist in the types map")
+        return cls.typemaps[driver]
