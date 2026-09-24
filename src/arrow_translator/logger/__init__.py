b2782451da6602ba6b152bd83c80c2ed9825c1d4
Dimos Konstantinos <konstantinos.dimos@themart.gr>
2026-09-24 17:00:38 +0300

    additions in the console handler

--- src/arrow_translator/logger/__init__.py
+++ src/arrow_translator/logger/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from .json_logging import JsonLinesFormatter
+from .formatters import ConsoleFormatter, JsonLinesFormatter
 
 DEFAULT_LOGGER_NAME = "arrow-translator"
 FILE_LOGGER = ".arrow-trans.log"
@@ -11,12 +11,7 @@ def _build_default_logger():
     file_handler = logging.FileHandler(encoding="utf-8", filename=FILE_LOGGER, mode="a")
     file_handler.setLevel(level=logging.DEBUG)
     console_handler = logging.StreamHandler()
-    console_formatter = logging.Formatter(
-        fmt="{asctime}{msecs} [{levelname}] {name} || File: {filename} Function: {funcName}:{lineno} - {message}",
-        style="{",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    console_handler.setFormatter(console_formatter)
+    console_handler.setFormatter(ConsoleFormatter())
     console_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(JsonLinesFormatter())
     logger.addHandler(console_handler)
@@ -29,17 +24,11 @@ _ARROW_TRANSLATOR_LOGGER_NAME = DEFAULT_LOGGER_NAME
 _ARROW_TRANSLATOR_DEFAULT_LOGGER = _build_default_logger()
 _ACTIVE_ARROW_TRANSLATOR_LOGGER = _ARROW_TRANSLATOR_DEFAULT_LOGGER
 
-def get_logger() -> logging.Logger:
-    return _ACTIVE_ARROW_TRANSLATOR_LOGGER
 
 def change_logger(logger: logging.Logger):
     global _ACTIVE_ARROW_TRANSLATOR_LOGGER, _ARROW_TRANSLATOR_LOGGER_NAME
     _ACTIVE_ARROW_TRANSLATOR_LOGGER, _ARROW_TRANSLATOR_LOGGER_NAME = logger, logger.name
-    if logger.name != DEFAULT_LOGGER_NAME:
-        logger.info(
-            "Arrow-Translator initiallized with the non default logger: %s", logger.name
-        )
 
 
-if get_logger().name == DEFAULT_LOGGER_NAME:
-    get_logger().info("Arrow-Translator Initialized with default Settings")
+def get_logger() -> logging.Logger:
+    return _ACTIVE_ARROW_TRANSLATOR_LOGGER
--- src/arrow_translator/logger/formatters.py
+++ src/arrow_translator/logger/formatters.py
@@ -27,18 +27,21 @@ PARAMETERS = {
     "processName",
 }
 
+
 class JsonLinesFormatter(Formatter):
     def format(self, record: LogRecord) -> str:
         message = {
             "timestamp": datetime.datetime.fromtimestamp(
-                record.created, tz=datetime.timezone.utc
-            ).isoformat(),
+                record.created,
+            )
+            .astimezone()
+            .isoformat(),
             "level": record.levelname,
             "name": record.name,
             "function": record.funcName,
             "module": record.module,
             "line_number": record.lineno,
-            "message": record.getMessage(),
+            "message": record.getMessage().split("|")[-1],
         }
 
         for _ in record.__dict__.keys():
@@ -50,3 +53,22 @@ class JsonLinesFormatter(Formatter):
         if record.stack_info:
             message["stack_info"] = self.formatStack(record.stack_info)
         return json.dumps(message)
+
+
+class ConsoleFormatter(Formatter):
+    def format(self, record: LogRecord) -> str:
+        _dt = datetime.datetime.fromtimestamp(record.created).astimezone().isoformat()
+        _lvl = record.levelname
+        _name = record.name
+        _func = record.funcName
+        _mod = record.module
+        _line_no = record.lineno
+        _msg = record.getMessage().split("|")[-1]
+
+        message = f"{_dt}|[{_lvl}]|[{_name}]|({_func} on {_mod}:{_line_no})|{_msg}"
+        if record.exc_info:
+            message += f"|Exception: {self.formatException(record.exc_info)}"
+        if record.stack_info:
+            message += f"|StackTrace: {self.formatStack(record.stack_info)}"
+
+        return message
